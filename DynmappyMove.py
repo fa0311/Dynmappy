3964d998f0a1d7c6dd64ecd6db60725893893c07
fa0311 <yukinashi2742756@yahoo.co.jp>
2021-05-30 15:04:26 +0900

    add config

--- DynmappyMove.py
+++ DynmappyMove.py
@@ -2,50 +2,39 @@ import Dynmappy
 
 
 class DynmappyMove:
-    def __init__(self):
+    def __init__(self, config):
         self.mapnum = 0
-        self.maplist = [
-            {
-                "name": "build_world",
-                "x": -5,
-                "y": -7
-            },
-            {
-                "name": "resource_world",
-                "x": -4,
-                "y": -3
-            }
-        ]
-        self.hnow = self.maplist[self.mapnum]["x"]
-        self.vnow = self.maplist[self.mapnum]["y"]
-        Dynmappy("plugins/dynmap/web/tiles/"+self.maplist[self.mapnum]["name"]+"/flat").output(
+        self.config = config
+        self.hnow = self.config[self.mapnum]["x"]
+        self.vnow = self.config[self.mapnum]["y"]
+        Dynmappy("plugins/dynmap/web/tiles/"+self.config[self.mapnum]["name"]+"/flat").output(
             self.hnow, self.hnow+8, self.vnow, self.vnow+6, 0, "dynmap.jpg", "assets/image/128x128.png")
 
     def up(self):
         self.vnow += 3
-        Dynmappy("plugins/dynmap/web/tiles/"+self.maplist[self.mapnum]["name"]+"/flat").output(
+        Dynmappy("plugins/dynmap/web/tiles/"+self.config[self.mapnum]["name"]+"/flat").output(
             self.hnow, self.hnow+8, self.vnow, self.vnow+6, 0, "dynmap.jpg", "assets/image/128x128.png")
 
     def down(self):
         self.vnow -= 3
-        Dynmappy("plugins/dynmap/web/tiles/"+self.maplist[self.mapnum]["name"]+"/flat").output(
+        Dynmappy("plugins/dynmap/web/tiles/"+self.config[self.mapnum]["name"]+"/flat").output(
             self.hnow, self.hnow+8, self.vnow, self.vnow+6, 0, "dynmap.jpg", "assets/image/128x128.png")
 
     def right(self):
         self.hnow += 4
-        Dynmappy("plugins/dynmap/web/tiles/"+self.maplist[self.mapnum]["name"]+"/flat").output(
+        Dynmappy("plugins/dynmap/web/tiles/"+self.config[self.mapnum]["name"]+"/flat").output(
             self.hnow, self.hnow+8, self.vnow, self.vnow+6, 0, "dynmap.jpg", "assets/image/128x128.png")
 
     def left(self):
         self.hnow -= 4
-        Dynmappy("plugins/dynmap/web/tiles/"+self.maplist[self.mapnum]["name"]+"/flat").output(
+        Dynmappy("plugins/dynmap/web/tiles/"+self.config[self.mapnum]["name"]+"/flat").output(
             self.hnow, self.hnow+8, self.vnow, self.vnow+6, 0, "dynmap.jpg", "assets/image/128x128.png")
 
     def map(self):
         self.mapnum += 1
-        if len(self.maplist) <= self.mapnum:
+        if len(self.config) <= self.mapnum:
             self.mapnum = 0
-        self.hnow = self.maplist[self.mapnum]["x"]
-        self.vnow = self.maplist[self.mapnum]["y"]
-        Dynmappy("plugins/dynmap/web/tiles/"+self.maplist[self.mapnum]["name"]+"/flat").output(
+        self.hnow = self.config[self.mapnum]["x"]
+        self.vnow = self.config[self.mapnum]["y"]
+        Dynmappy("plugins/dynmap/web/tiles/"+self.config[self.mapnum]["name"]+"/flat").output(
             self.hnow, self.hnow+8, self.vnow, self.vnow+6, 0, "dynmap.jpg", "assets/image/128x128.png")
